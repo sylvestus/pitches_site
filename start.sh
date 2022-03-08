3e39dc9eff18a94bc636b2c9c3699d2e175a0194
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-03-08 16:42:09 +0300

    or app entry

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME='silvanussigei19960@gmail.com'
+export MAIL_PASSWORD='Id34306488'
+export SECRET_KEY='silvano36'
+export DATABASE_URL='postgresql+psycopg2://moringa:access@localhost/pitches'
+python3 manage.py server
