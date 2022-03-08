f49f64f74f7446710ebadf2426332184284b829c
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-03-08 16:41:30 +0300

    add manage file that runs our app and registers flask xtensios and blueprints initialized in the root init

---
+++ manage.py
@@ -0,0 +1,20 @@
+from multiprocessing import managers
+from app import create_app,db
+import app
+from app.models import User,Comments,PhotoProfile,Pitches
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
+
+# creating app instance
+app = create_app('development')
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+app = create_app('test')
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitches = Pitches,Comments = Comments)
+if __name__ == '__main__':
+    manager.run()
